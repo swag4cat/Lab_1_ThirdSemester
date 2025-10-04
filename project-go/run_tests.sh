2e362f3ebb72179c59ef1b60889fc5a1bd2d2f91
swag4cat <semyon.kondratov@yandex.ru>
2025-10-04 20:51:00 +0700

    Updeted run_tests.sh

--- project-go/run_tests.sh
+++ project-go/run_tests.sh
@@ -24,26 +24,7 @@ echo "======================="
 ./main --file test_array.txt --query "PRINTM arr"
 
 echo ""
-echo "2. ТЕСТИРОВАНИЕ ДЕРЕВА"
-echo "======================"
-./main --file test_tree.txt --query "TINSERT tree 50"
-./main --file test_tree.txt --query "TINSERT tree 30"
-./main --file test_tree.txt --query "TINSERT tree 70"
-./main --file test_tree.txt --query "TINSERT tree 20"
-./main --file test_tree.txt --query "TINSERT tree 40"
-./main --file test_tree.txt --query "TINSERT tree 60"
-./main --file test_tree.txt --query "TINSERT tree 80"
-./main --file test_tree.txt --query "PRINT tree"
-./main --file test_tree.txt --query "TGET tree 60"
-./main --file test_tree.txt --query "TGET tree 99"        # ошибка
-./main --file test_tree.txt --query "TDEL tree 20"
-./main --file test_tree.txt --query "TDEL tree 30"
-./main --file test_tree.txt --query "TDEL tree 50"
-./main --file test_tree.txt --query "TDEL tree 999"       # ошибка
-./main --file test_tree.txt --query "PRINT tree"
-
-echo ""
-echo "3. ТЕСТИРОВАНИЕ ОДНОСВЯЗНОГО СПИСКА"
+echo "2. ТЕСТИРОВАНИЕ ОДНОСВЯЗНОГО СПИСКА"
 echo "==================================="
 ./main --file test_slist.txt --query "FPUSH slist 10"
 ./main --file test_slist.txt --query "FPUSH slist 30"
@@ -62,7 +43,7 @@ echo "==================================="
 ./main --file test_slist.txt --query "PRINTF slist"
 
 echo ""
-echo "4. ТЕСТИРОВАНИЕ ДВУСВЯЗНОГО СПИСКА"
+echo "3. ТЕСТИРОВАНИЕ ДВУСВЯЗНОГО СПИСКА"
 echo "=================================="
 ./main --file test_dlist.txt --query "LPUSH dlist 100"
 ./main --file test_dlist.txt --query "LPUSH dlist 300"
@@ -79,7 +60,7 @@ echo "=================================="
 ./main --file test_dlist.txt --query "PRINTL dlist"
 
 echo ""
-echo "5. ТЕСТИРОВАНИЕ СТЕКА"
+echo "4. ТЕСТИРОВАНИЕ СТЕКА"
 echo "====================="
 ./main --file test_stack.txt --query "SPUSH stack 1"
 ./main --file test_stack.txt --query "SPUSH stack 2"
@@ -92,7 +73,7 @@ echo "====================="
 ./main --file test_stack.txt --query "SPOP stack"         # ошибка
 
 echo ""
-echo "6. ТЕСТИРОВАНИЕ ОЧЕРЕДИ"
+echo "5. ТЕСТИРОВАНИЕ ОЧЕРЕДИ"
 echo "======================="
 ./main --file test_queue.txt --query "QPUSH queue 10"
 ./main --file test_queue.txt --query "QPUSH queue 20"
@@ -105,7 +86,7 @@ echo "======================="
 ./main --file test_queue.txt --query "QPOP queue"         # ошибка
 
 echo ""
-echo "7. ТЕСТ НЕИЗВЕСТНОЙ КОМАНДЫ"
+echo "6. ТЕСТ НЕИЗВЕСТНОЙ КОМАНДЫ"
 echo "==========================="
 ./main --file test_array.txt --query "FOO bar xyz"        # ошибка
 
